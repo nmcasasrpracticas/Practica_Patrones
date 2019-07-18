41d3ed961b3081fe1438e3f37e0120ca92a506d9
nmcasasr <38595223+nmcasasr@users.noreply.github.com>
2019-07-18 14:35:08 -0500

    Flyweight

--- Main.java
+++ Main.java
@@ -1,183 +1,57 @@
-interface I {
-  void doIt();
+import java.util.HashMap;
+interface Transporte {
+   void draw();
+}
+class Carro implements Transporte {
+   private String tipo;
+   private String color;
+  
+
+   public Carro(String tipo){
+      this.tipo = tipo;		
+   }
+
+   public void setColor(String color) {
+      this.color = color;
+   }
+
+
+   @Override
+   public void draw() {
+      System.out.println("Es un carro de tipo " + tipo + " de color " + color );
+   }
 }
 
-class Carro implements I {
-  public void doIt() {
-    System.out.print("Carro con ");
-  }
+class ShapeFactory {
+
+   private static final HashMap<String,Carro> carroMap = new HashMap<String,Carro>();
+
+   public static Transporte getCarro(String tipo) {
+      Carro carro = (Carro)carroMap.get(tipo);
+
+      if(carro == null) {
+         carro = new Carro(tipo);
+         carroMap.put(tipo, carro);
+         System.out.println("Creando carro de tipo : " + tipo);
+      }
+      return carro;
+   }
 }
-
-abstract class D implements I {
-  private I core;
-
-  public D(I inner) {
-    core = inner;
-  }
-
-  public void doIt() {
-    core.doIt();
-  }
-}
-
-class Nitro extends D {
-  public Nitro(I inner) {
-    super(inner);
-  }
-
-  public void doIt() {
-    super.doIt();
-    doNitro();
-  }
-
-  private void doNitro() {
-    System.out.print("nitro, ");
-  }
-}
-
-class Neon extends D {
-  public Neon(I inner) {
-    super(inner);
-  }
-
-  public void doIt() {
-    super.doIt();
-    doNeon();
-  }
-
-  private void doNeon() {
-    System.out.print("neon, ");
-  }
-}
-
-class Alarma extends D {
-  public Alarma(I inner) {
-    super(inner);
-  }
-
-  public void doIt() {
-    super.doIt();
-    doAlarma();
-  }
-
-  private void doAlarma() {
-    System.out.print("alarma, ");
-  }
-}
-
-class Aire extends D {
-  public Aire(I inner) {
-    super(inner);
-  }
-
-  public void doIt() {
-    super.doIt();
-    doAire();
-  }
-
-  private void doAire() {
-    System.out.print("aire acondicionado, ");
-  }
-}
-
-class Turbo extends D {
-  public Turbo(I inner) {
-    super(inner);
-  }
-
-  public void doIt() {
-    super.doIt();
-    doTurbo();
-  }
-
-  private void doTurbo() {
-    System.out.print("turbo, ");
-  }
-}
-
-class Oxigeno extends D {
-  public Oxigeno(I inner) {
-    super(inner);
-  }
-
-  public void doIt() {
-    super.doIt();
-    doOxigeno();
-  }
-
-  private void doOxigeno() {
-    System.out.print("oxigeno, ");
-  }
-}
-
-class Rines extends D {
-  public Rines(I inner) {
-    super(inner);
-  }
-
-  public void doIt() {
-    super.doIt();
-    doRines();
-  }
-
-  private void doRines() {
-    System.out.print("rines, ");
-  }
-}
-
-
-class FibraCarbono extends D {
-  public FibraCarbono(I inner) {
-    super(inner);
-  }
-
-  public void doIt() {
-    super.doIt();
-    doFibraCarbono();
-  }
-
-  private void doFibraCarbono() {
-    System.out.print("fibra de carbono, ");
-  }
-}
-
-class Camaras extends D {
-  public Camaras(I inner) {
-    super(inner);
-  }
-
-  public void doIt() {
-    super.doIt();
-    doCamaras();
-  }
-
-  private void doCamaras() {
-    System.out.print("camaras para parquear, ");
-  }
-}
-
-class Elon extends D {
-  public Elon(I inner) {
-    super(inner);
-  }
-
-  public void doIt() {
-    super.doIt();
-    doElon();
-  }
-
-  private void doElon() {
-    System.out.print("el mismisimo Elon Musk, ");
-  }
-}
-
 class Main {
-  public static void main(String[] args) {
-    //Decorator
-    I[] array = { new Elon(new Carro()), new Neon(new Nitro(new Carro())), new Camaras(new Turbo(new Oxigeno(new Carro()))) };
-    for (I anArray : array) {
-      anArray.doIt();
-      System.out.print("  ");
-    }
-  }
+   static String tipos[] = { "gas", "electrico", "hibrido", "combustible"};
+   static String colores[] = { "rojo", "verde", "azul"};
+   public static void main(String[] args) {
+
+      for(int i=0; i < 20; ++i) {
+         Carro carro = (Carro)ShapeFactory.getCarro(getRandomTipo());
+         carro.setColor(getRandomColor());
+         carro.draw();
+      }
+   }
+   private static String getRandomTipo() {
+      return tipos[(int)(Math.random()*tipos.length)];
+   }
+   private static String getRandomColor() {
+      return colores[(int)(Math.random()*colores.length)];
+   }
 }
